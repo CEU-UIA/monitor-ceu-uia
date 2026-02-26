335ad34d769fc716dd61cc147c5439c6dfd3638b
CEU-UIA <ceu@uia.org.ar>
2026-02-26 17:12:22 -0300

    Update ipi.py

--- pages/ipi.py
+++ pages/ipi.py
@@ -1242,6 +1242,8 @@ def render_ipi(go_to):
             unsafe_allow_html=True,
         )
 
+        st.markdown("<div style='height:40px;'></div>", unsafe_allow_html=True)
+
         if "ipi_modal_open" not in st.session_state:
             st.session_state["ipi_modal_open"] = False
             st.session_state["ipi_modal_div_name"] = None
